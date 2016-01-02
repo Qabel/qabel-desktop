3627740b2844ee42ccabe72fc416f4820a453ba5
Julian Seeger <seeger.julian@gmail.com>
2016-01-02 14:46:16 +0100

    #72 standalone build using qabel-core project

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -x
 bash start-servers.sh
 bash gradlew test
 bash stop-servers.sh
--- jni/curve25519.h
+++
@@ -1,2 +0,0 @@
-extern int crypto_scalarmult(unsigned char *,const unsigned char *,const unsigned char *);
-extern int crypto_scalarmult_base(unsigned char *,const unsigned char *);
