c916e7261443322ebf87cf07be10ad06b0e025d4
Julian Seeger <seeger.julian@gmail.com>
2016-01-09 01:43:35 +0100

    navi / base layout and integrationpoint for future views

--- src/main/java/de/qabel/desktop/ui/accounting/AccountingView.java
+++ src/main/java/de/qabel/desktop/ui/accounting/AccountingView.java
@@ -1,6 +1,6 @@
 package de.qabel.desktop.ui.accounting;
 
-import com.airhacks.afterburner.views.FXMLView;
+import com.airhacks.afterburner.views.QabelFXMLView;
 
-public class AccountingView extends FXMLView {
+public class AccountingView extends QabelFXMLView {
 }
