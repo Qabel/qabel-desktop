cfddbb1bc7e4161c3e5d268d012681f86bc0de4f
Markus Thimm <markus.thimm@googlemail.com>
2016-01-11 11:30:13 +0100

    change PopupMenu to JPopUpMenu

--- src/main/java/de/qabel/desktop/DesktopClient.java
+++ src/main/java/de/qabel/desktop/DesktopClient.java
@@ -21,110 +21,150 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.event.*;
 import java.net.URISyntaxException;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.*;
 
 
 public class DesktopClient extends Application {
-	private static final String TITLE = "Qabel Desktop Client";
+    private static final String TITLE = "Qabel Desktop Client";
+    Scene scene;
 
-	public static void main(String[] args) throws Exception {
-		launch(args);
-	}
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		setUserAgentStylesheet(STYLESHEET_MODENA);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        setUserAgentStylesheet(STYLESHEET_MODENA);
 
-		final Map<String, Object> customProperties = new HashMap<>();
-		ClientConfiguration config = initDiContainer(customProperties);
+        final Map<String, Object> customProperties = new HashMap<>();
+        ClientConfiguration config = initDiContainer(customProperties);
 
-		Scene scene;
-		if (!config.hasAccount()) {
-			scene = new Scene(new LoginView().getView(), 370, 530);
-			config.addObserver((o, arg) -> {
-				if (arg instanceof Account) {
-					Scene layoutScene = new Scene(new LayoutView().getView(), 800, 600);
-					Platform.runLater(() -> primaryStage.setScene(layoutScene));
-				}
-			});
-		} else {
-			scene = new Scene(new LayoutView().getView(), 800, 600);
-		}
+        if (!config.hasAccount()) {
+            scene = new Scene(new LoginView().getView(), 370, 530);
+            config.addObserver((o, arg) -> {
+                if (arg instanceof Account) {
+                    Scene layoutScene = new Scene(new LayoutView().getView(), 800, 600);
+                    Platform.runLater(() -> primaryStage.setScene(layoutScene));
+                }
+            });
+        } else {
+            scene = new Scene(new LayoutView().getView(), 800, 600);
+        }
 
-		primaryStage.setScene(scene);
+        Platform.setImplicitExit(false);
+        primaryStage.setScene(scene);
+        setTrayIcon(primaryStage);
+        primaryStage.setTitle(TITLE);
+        primaryStage.show();
+    }
 
-		setTrayIcon(primaryStage);
-		primaryStage.setTitle(TITLE);
-		primaryStage.show();
-	}
+    private ClientConfiguration initDiContainer(Map<String, Object> customProperties) throws QblInvalidEncryptionKeyException, URISyntaxException {
+        Persistence<String> persistence = new SQLitePersistence("db.sqlite", "qabel".toCharArray(), 65536);
+        customProperties.put("persistence", persistence);
+        customProperties.put("dropUrlGenerator", new DropUrlGenerator("http://localhost:5000"));
+        PersistenceIdentityRepository identityRepository = new PersistenceIdentityRepository(persistence);
+        customProperties.put("identityRepository", identityRepository);
+        PersistenceAccountRepository accountRepository = new PersistenceAccountRepository(persistence);
+        customProperties.put("accountRepository", accountRepository);
+        ClientConfiguration clientConfig = getClientConfiguration(persistence, identityRepository, accountRepository);
+        customProperties.put("clientConfiguration", clientConfig);
 
-	private ClientConfiguration initDiContainer(Map<String, Object> customProperties) throws QblInvalidEncryptionKeyException, URISyntaxException {
-		Persistence<String> persistence = new SQLitePersistence("db.sqlite", "qabel".toCharArray(), 65536);
-		customProperties.put("persistence", persistence);
-		customProperties.put("dropUrlGenerator", new DropUrlGenerator("http://localhost:5000"));
-		PersistenceIdentityRepository identityRepository = new PersistenceIdentityRepository(persistence);
-		customProperties.put("identityRepository", identityRepository);
-		PersistenceAccountRepository accountRepository = new PersistenceAccountRepository(persistence);
-		customProperties.put("accountRepository", accountRepository);
-		ClientConfiguration clientConfig = getClientConfiguration(persistence, identityRepository, accountRepository);
-		customProperties.put("clientConfiguration", clientConfig);
+        Injector.setConfigurationSource(customProperties::get);
+        Injector.setInstanceSupplier(new RecursiveInjectionInstanceSupplier(customProperties));
+        return clientConfig;
+    }
 
-		Injector.setConfigurationSource(customProperties::get);
-		Injector.setInstanceSupplier(new RecursiveInjectionInstanceSupplier(customProperties));
-		return clientConfig;
-	}
+    private ClientConfiguration getClientConfiguration(Persistence<String> persistence, IdentityRepository identityRepository, AccountRepository accountRepository) {
+        ClientConfigurationRepository repo = new PersistenceClientConfigurationRepository(
+                persistence,
+                new ClientConfigurationFactory(),
+                identityRepository,
+                accountRepository
+        );
+        final ClientConfiguration config = repo.load();
+        config.addObserver((o, arg) -> {
+            repo.save(config);
+        });
+        return config;
+    }
 
-	private ClientConfiguration getClientConfiguration(Persistence<String> persistence, IdentityRepository identityRepository, AccountRepository accountRepository) {
-		ClientConfigurationRepository repo = new PersistenceClientConfigurationRepository(
-				persistence,
-				new ClientConfigurationFactory(),
-				identityRepository,
-				accountRepository
-		);
-		final ClientConfiguration config = repo.load();
-		config.addObserver((o, arg) -> {
-			repo.save(config);
-		});
-		return config;
-	}
+    private void setTrayIcon(Stage primaryStage) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
 
-	private void setTrayIcon(Stage primaryStage) {
 
-		SystemTray sTray = SystemTray.getSystemTray();
+        SystemTray sTray = SystemTray.getSystemTray();
+        primaryStage.setOnCloseRequest(arg0 -> primaryStage.hide());
 
-		ActionListener listenerShow = e -> Platform.runLater(() -> primaryStage.show());
-		ActionListener listenerClose = e -> System.exit(0);
-		primaryStage.setOnCloseRequest(arg0 -> primaryStage.hide());
+        final JPopupMenu popup = buildSystemTrayJPopupMenu(primaryStage);
 
-		PopupMenu popup = new PopupMenu();
-		MenuItem showItem = new MenuItem("Öffnen");
-		MenuItem exitItem = new MenuItem("Beenden");
 
-		showItem.addActionListener(listenerShow);
-		exitItem.addActionListener(listenerClose);
 
-		popup.add(showItem);
-		popup.add(exitItem);
+        URL url = System.class.getResource("/logo.png");
+        Image img = Toolkit.getDefaultToolkit().getImage(url);
+        TrayIcon icon = new TrayIcon(img, "Qabel", null);
+        icon.setImageAutoSize(true);
 
-		URL url = System.class.getResource("/logo.png");
-		Image img = Toolkit.getDefaultToolkit().getImage(url);
-		TrayIcon icon = new TrayIcon(img, "Qabel", popup);
-		icon.setImageAutoSize(true);
+        trayIconListener(popup, icon);
 
-		try {
-			sTray.add(icon);
-		} catch (AWTException e) {
-			System.err.println(e);
-		}
 
-	}
+        try {
+            sTray.add(icon);
+        } catch (AWTException e) {
+            System.err.println(e);
+        }
 
-	public static Account getBoxAccount() {
-		return new Account("http://localhost:9696", "testuser", "testuser");
-	}
+    }
+
+    private void trayIconListener(final JPopupMenu popup, TrayIcon icon) {
+        icon.addMouseListener(new MouseAdapter() {
+            boolean visible = false;
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    if (!visible) {
+                        popup.setLocation(e.getX(), e.getY() + 10);
+                        popup.setVisible(true);
+                        visible = true;
+                        visible = true;
+                    } else {
+                        popup.setVisible(false);
+                        visible = false;
+                    }
+                }
+            }
+        });
+    }
+
+    protected JPopupMenu buildSystemTrayJPopupMenu(Stage primaryStage) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        final JPopupMenu menu = new JPopupMenu();
+        final JMenuItem showMenuItem = new JMenuItem("Show");
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+
+        ActionListener listener = ae -> {
+            Object source = ae.getSource();
+            if (source == showMenuItem) {
+                Platform.runLater(primaryStage::show);
+            } else if (source == exitMenuItem) {
+                System.exit(0);
+            }
+        };
+
+        for (JMenuItem item : new JMenuItem[]{showMenuItem, exitMenuItem}) {
+            if (item == exitMenuItem) menu.addSeparator();
+            menu.add(item);
+            item.addActionListener(listener);
+        }
+        return menu;
+    }
+
+    public static Account getBoxAccount() {
+        return new Account("http://localhost:9696", "testuser", "testuser");
+    }
 
 }
