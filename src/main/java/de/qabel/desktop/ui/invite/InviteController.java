6a5c9438a9cac9c80e9570930dbe231166644644
Markus Thimm <markus.thimm@googlemail.com>
2016-01-20 18:06:35 +0100

    email invation without tests

--- src/main/java/de/qabel/desktop/ui/invite/InviteController.java
+++ src/main/java/de/qabel/desktop/ui/invite/InviteController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextArea;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,23 +20,53 @@ public class InviteController extends AbstractController implements Initializabl
 	@FXML
 	private TextArea textarea;
 
+	private ResourceBundle bundle;
 	public void initialize(URL location, ResourceBundle resources) {
-		textarea.setText(resources.getString("inviteText"));
+
+		this.bundle = resources;
+		textarea.setText(bundle.getString("inviteText"));
 		textarea.setEditable(false);
 	}
 
 	@FXML
 	protected void handleInviteButtonAction(ActionEvent event) throws IOException {
-		Desktop desktop;
-		if (Desktop.isDesktopSupported()
-				&& (desktop = Desktop.getDesktop()).isSupported(Desktop.Action.MAIL)) {
-			String message = "mailTo:markus.thimm@gmail.com" + "?subject=" + "TEST%20SUBJECT"
-					+ "&body=" + "TEST%20BODY";
-			URI uri = URI.create(message);
-			desktop.browse(uri);
+		new Thread(() -> {
+			Desktop desktop;
+			if (Desktop.isDesktopSupported()
+					&& (desktop = Desktop.getDesktop()).isSupported(Desktop.Action.MAIL)) {
+
+				String mailTo = "";
+				String subject = createEMailSubject();
+				String body = createEMailBody();
+				String cc = "";
+
+				String mailURIStr = String.format("mailto:%s?subject=%s&cc=%s&body=%s",
+						mailTo, subject, cc, body);
+
+				try {
+					desktop.mail( new URI(mailURIStr));
+				} catch (URISyntaxException | IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
+		).start();
 
 	}
 
+	private String createEMailBody() {
+		String body =  bundle.getString("inviteText");
+		body = body.replace("\n", "%0D%0A");
+		body = body.replace(" ", "%20");
+		return body;
+	}
+
+	private String createEMailSubject() {
+		String subject = bundle.getString("emailSubjectText");
+		subject = subject.replace(" ", "%20");
+		return subject;
+	}
 
 }
+
+
