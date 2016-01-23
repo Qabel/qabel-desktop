fd1adc90f124573ec998b18ccf0b0d534d1af061
Julian Seeger <seeger.julian@gmail.com>
2016-01-23 02:23:10 +0100

    #44 refactored DefaultSyncer

--- src/main/java/de/qabel/desktop/daemon/sync/worker/DefaultSyncer.java
+++ src/main/java/de/qabel/desktop/daemon/sync/worker/DefaultSyncer.java
@@ -5,15 +5,15 @@ import de.qabel.desktop.daemon.management.BoxSyncBasedDownload;
 import de.qabel.desktop.daemon.management.BoxSyncBasedUpload;
 import de.qabel.desktop.daemon.management.LoadManager;
 import de.qabel.desktop.daemon.sync.event.ChangeEvent;
-import de.qabel.desktop.daemon.sync.event.WatchEvent;
 import de.qabel.desktop.exceptions.QblStorageException;
-import de.qabel.desktop.storage.BoxVolume;
 import de.qabel.desktop.storage.cache.CachedBoxVolume;
-import org.apache.log4j.spi.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class DefaultSyncer implements Syncer {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private CachedBoxVolume boxVolume;
 	private BoxSyncConfig config;
 	private LoadManager manager;
@@ -31,19 +31,40 @@ public class DefaultSyncer implements Syncer {
 
 	@Override
 	public void run() {
-		watcher = new TreeWatcher(config.getLocalPath(), watchEvent -> {
-			if (!watchEvent.isValid()) {
-				return;
-			}
-			String type = "";
-			if (watchEvent instanceof ChangeEvent)
-				type = ((ChangeEvent)watchEvent).getType().toString();
-			System.out.println(this.hashCode() + ": local update " + type + " " + watchEvent.getPath().toString());
-			manager.addUpload(new BoxSyncBasedUpload(boxVolume, config, watchEvent));
-		});
-		watcher.setDaemon(true);
-		watcher.start();
+		startWatcher();
+		registerRemoteChangeHandler();
+		registerRemotePoller();
 
+		try {
+			boxVolume.navigate().notifyAllContents();
+		} catch (QblStorageException e) {
+			logger.warn(e.getMessage(), e);
+		}
+	}
+
+	protected void registerRemotePoller() {
+		poller = new Thread(() -> {
+			try {
+				while (!Thread.interrupted()) {
+					try {
+						boxVolume.navigate().refresh();
+						polling = true;
+					} catch (QblStorageException e) {
+						e.printStackTrace();
+					}
+					Thread.sleep(pollUnit.toMillis(pollInterval));
+				}
+			} catch (InterruptedException e) {
+				logger.debug("poller stopped");
+			} finally {
+				polling = false;
+			}
+		});
+		poller.setDaemon(true);
+		poller.start();
+	}
+
+	protected void registerRemoteChangeHandler() {
 		try {
 			if (boxVolume.navigate() != null) {
 				boxVolume.navigate().addObserver((o, arg) -> {
@@ -58,31 +79,21 @@ public class DefaultSyncer implements Syncer {
 		} catch (QblStorageException e) {
 			throw new IllegalStateException("Failed to watch remote dir: " + e.getMessage(), e);
 		}
+	}
 
-		poller = new Thread(() -> {
-			try {
-				while (!Thread.interrupted()) {
-					try {
-						boxVolume.navigate().refresh();
-						polling = true;
-					} catch (QblStorageException e) {
-						e.printStackTrace();
-					}
-					Thread.sleep(pollUnit.toMillis(pollInterval));
-				}
-			} catch (InterruptedException e) {
-				org.slf4j.LoggerFactory.getLogger(getClass()).debug("poller stopped");
-			} finally {
-				polling = false;
+	protected void startWatcher() {
+		watcher = new TreeWatcher(config.getLocalPath(), watchEvent -> {
+			if (!watchEvent.isValid()) {
+				return;
 			}
+			String type = "";
+			if (watchEvent instanceof ChangeEvent)
+				type = ((ChangeEvent)watchEvent).getType().toString();
+			System.out.println(this.hashCode() + ": local update " + type + " " + watchEvent.getPath().toString());
+			manager.addUpload(new BoxSyncBasedUpload(boxVolume, config, watchEvent));
 		});
-		poller.setDaemon(true);
-		poller.start();
-		try {
-			boxVolume.navigate().notifyAllContents();
-		} catch (QblStorageException e) {
-			org.slf4j.LoggerFactory.getLogger(getClass()).warn(e.getMessage(), e);
-		}
+		watcher.setDaemon(true);
+		watcher.start();
 	}
 
 	@Override
