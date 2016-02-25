9d0b999380de647ef37bac47486ee273f24d438e
Julian Seeger <seeger.julian@gmail.com>
2016-02-25 23:35:52 +0100

    stabalized tests

--- src/test-gui/java/de/qabel/desktop/ui/remotefs/FilterableFolderTreeItemTest.java
+++ src/test-gui/java/de/qabel/desktop/ui/remotefs/FilterableFolderTreeItemTest.java
@@ -1,26 +1,25 @@
 package de.qabel.desktop.ui.remotefs;
 
-import com.sun.javafx.robot.impl.BaseFXRobot;
+import com.airhacks.afterburner.views.FXMLView;
 import de.qabel.desktop.daemon.sync.worker.BoxNavigationStub;
+import de.qabel.desktop.daemon.sync.worker.BoxVolumeStub;
+import de.qabel.desktop.exceptions.QblStorageException;
 import de.qabel.desktop.storage.BoxFile;
 import de.qabel.desktop.storage.BoxFolder;
 import de.qabel.desktop.storage.BoxObject;
 import de.qabel.desktop.ui.AbstractControllerTest;
+import de.qabel.desktop.ui.AbstractGuiTest;
 import javafx.beans.property.StringProperty;
-import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import javafx.scene.layout.HBox;
 import org.junit.Before;
 import org.junit.Test;
-import org.testfx.api.FxRobot;
 
 import java.nio.file.Paths;
 
 import static de.qabel.desktop.daemon.sync.event.ChangeEvent.TYPE.DELETE;
 import static org.junit.Assert.*;
 
-public class FilterableFolderTreeItemTest extends AbstractControllerTest {
+public class FilterableFolderTreeItemTest extends AbstractGuiTest<RemoteFSController> {
 	private FilterableFolderTreeItem folderTree;
 	private StringProperty filter;
 	private BoxFile subFile;
@@ -28,9 +27,8 @@ public class FilterableFolderTreeItemTest extends AbstractControllerTest {
 	private BoxFolder subFolder;
 	private BoxNavigationStub navigation;
 
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
+	@Override
+	protected FXMLView getView() {
 		subFile = new BoxFile("prefix", "block", "fileName", 0L, 0L, new byte[0]);
 		subSubFile = new BoxFile("prefix", "block", "innerFileName", 0L, 0L, new byte[0]);
 		subFolder = new BoxFolder("prefix", "folderName", new byte[0]);
@@ -40,16 +38,25 @@ public class FilterableFolderTreeItemTest extends AbstractControllerTest {
 		navigation = new BoxNavigationStub(indexNav, Paths.get("/folder"));
 		navigation.files.add(subFile);
 		navigation.folders.add(subFolder);
-		navigation.navigate("folderName").files.add(subSubFile);
+		try {
+			navigation.navigate("folderName").files.add(subSubFile);
+		} catch (QblStorageException e) {
+			fail(e.getMessage());
+		}
 
 		folderTree = new FilterableFolderTreeItem(folder, navigation);
+		folderTree.getChildren();
 		filter = folderTree.filterProperty();
+		((BoxVolumeStub)boxVolumeFactory.boxVolume).rootNavigation = navigation;
 		expectChildren(2);
-		runLaterAndWait(() -> {});
+		waitForUI();
+
+		return new RemoteFSView();
 	}
 
 	@Test
 	public void showsOriginalChildrenWithoutFilter() throws Exception {
+		Thread.sleep(10000);
 		expectChildren(2);
 		assertEquals("folderName", folderTree.getChildren().get(0).getValue().getName());
 		assertEquals("fileName", folderTree.getChildren().get(1).getValue().getName());
@@ -73,8 +80,14 @@ public class FilterableFolderTreeItemTest extends AbstractControllerTest {
 	}
 
 	private void expectChildren(int count) {
+		waitForUI();
+		waitUntil(() -> folderTree.getChildren().size() == count, 5000L, () -> {
+			return "expected " + count + " children but got " + folderTree.getChildren();
+		});
+	}
+
+	private void waitForUI() {
 		runLaterAndWait(() -> {});
-		waitUntil(() -> folderTree.getChildren().size() == count, 5000L, () -> "expected " + count + " children but got " + folderTree.getChildren());
 	}
 
 	@Test
@@ -86,6 +99,10 @@ public class FilterableFolderTreeItemTest extends AbstractControllerTest {
 
 		navigation.navigate("folderName").files.remove(subSubFile);
 		navigation.navigate("folderName").pushNotification(subSubFile, DELETE);
+
+
+		waitForUI();
+		waitUntil(() -> folderTree.getChildren().get(0).getChildren().isEmpty());
 
 		filter.setValue("inner");
 		expectChildren(0);
