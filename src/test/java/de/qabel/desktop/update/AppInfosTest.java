328efcaf65e726df80aed9d075adf464ecb7914a
Julian Seeger <seeger.julian@gmail.com>
2016-03-02 10:41:18 +0100

    completed appinfo parsing test

--- src/test/java/de/qabel/desktop/update/AppInfosTest.java
+++ src/test/java/de/qabel/desktop/update/AppInfosTest.java
@@ -16,8 +16,8 @@ public class AppInfosTest {
 				"            \"downloadURL\": \"http://m.qabel.de/apps/android.html\"\n" +
 				"        },\n" +
 				"        \"desktop\": {\n" +
-				"            \"currentAppVersion\": 91,\n" +
-				"            \"minimumAppVersion\": 89,\n" +
+				"            \"currentAppVersion\": \"0.2.0\",\n" +
+				"            \"minimumAppVersion\": \"0.2.0\",\n" +
 				"            \"downloadURL\": \"http://m.qabel.de/apps/desktop\"\n" +
 				"        }\n" +
 				"    }\n" +
@@ -26,5 +26,11 @@ public class AppInfosTest {
 		VersionInformation info = new Gson().fromJson(response, VersionInformation.class);
 
 		assertNotNull(info);
+		assertNotNull(info.getAppinfos());
+		assertNotNull(info.getAppinfos().getDesktop());
+		LatestVersionInfo desktopVersion = info.getAppinfos().getDesktop();
+		assertEquals("0.2.0", desktopVersion.getCurrentAppVersion());
+		assertEquals("0.2.0", desktopVersion.getMinimumAppVersion());
+		assertEquals("http://m.qabel.de/apps/desktop", desktopVersion.getDownloadURL());
 	}
 }
