36cfccec1feaa6f8775fc42870529d1b41d969f0
Julian Seeger <seeger.julian@gmail.com>
2016-03-10 18:13:33 +0100

    build with differend display

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 bash start-servers.sh
-xvfb-run -s "-screen 0 1280x1024x8" bash gradlew test guiTest distZip
+xvfb-run -s "-screen 1 1280x1024x8" bash gradlew test guiTest distZip
 bash stop-servers.sh
 
