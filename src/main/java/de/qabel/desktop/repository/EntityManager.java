7a06a5130dd375f5897085b0ec03296ca0c23ca6
Julian Seeger <seeger.julian@gmail.com>
2016-04-15 16:43:37 +0200

    #325 optimized parameter naming in EntityManager

--- src/main/java/de/qabel/desktop/repository/EntityManager.java
+++ src/main/java/de/qabel/desktop/repository/EntityManager.java
@@ -7,30 +7,31 @@ import java.util.WeakHashMap;
 public class EntityManager {
     private WeakHashMap<Class, WeakHashMap<Integer, Object>> entities = new WeakHashMap<>();
 
-    public boolean contains(Class clazz, int id) {
-        if (!entities.containsKey(clazz)) {
+    public boolean contains(Class entityType, int id) {
+        if (!entities.containsKey(entityType)) {
             return false;
         }
-        return entities.get(clazz).containsKey(id);
+        return entities.get(entityType).containsKey(id);
     }
 
-    public synchronized <T> void put(Class<T> clazz, SyncSettingItem entity) {
-        put(clazz, entity, entity.getId());
+    public synchronized <T> void put(Class<T> entityType, SyncSettingItem entity) {
+        put(entityType, entity, entity.getId());
     }
 
-    public synchronized <T> void put(Class<T> clazz, HasId entity) {
-        put(clazz, entity, entity.getId());
+    public synchronized <T> void put(Class<T> entityType, HasId entity) {
+        put(entityType, entity, entity.getId());
     }
 
-    public <T> void put(Class<T> clazz, Object entity, int id) {
-        if (!entities.containsKey(clazz)) {
-            entities.put(clazz, new WeakHashMap<>());
+    public <T> void put(Class<T> entityType, Object entity, int id) {
+        if (!entities.containsKey(entityType)) {
+            entities.put(entityType, new WeakHashMap<>());
         }
-        entities.get(clazz).put(id, entity);
+        entities.get(entityType).put(id, entity);
     }
 
-    public <T> T get(Class<T> clazz, int id) {
-        return (T)entities.get(clazz).get(id);
+    @SuppressWarnings("unchecked")
+    public <T> T get(Class<T> entityType, int id) {
+        return (T)entities.get(entityType).get(id);
     }
 
     public void clear() {
