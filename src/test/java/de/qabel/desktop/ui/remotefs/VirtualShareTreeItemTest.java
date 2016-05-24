71e86775ace707805ec98f9c90c431580140237a
julianseeger <seeger.julian@gmail.com>
2016-05-24 09:02:45 +0200

    #368 cleaned up VirtualShareTreeItemTest

--- src/test/java/de/qabel/desktop/ui/remotefs/VirtualShareTreeItemTest.java
+++ src/test/java/de/qabel/desktop/ui/remotefs/VirtualShareTreeItemTest.java
@@ -21,33 +21,27 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.*;
 
 public class VirtualShareTreeItemTest extends AbstractFxTest {
+    public static final long SECOND_SHARE_SIZE = 200L;
+    public static final long FIRST_SHARE_SIZE = 100L;
     private VirtualShareTreeItem item;
     private StubSharingService sharingService = new StubSharingService();
     private AuthenticatedDownloader readBackend;
     private ShareNotifications notifications = new ShareNotifications();
     private BoxObject value = new FakeBoxObject("test");
     private Node graphic = new Label("test");
-    private BoxExternalFile share;
-    private BoxExternalFile share2;
 
     @Before
     public void setUp() throws Exception {
         readBackend = new LocalReadBackend(Files.createTempDirectory("qbl_test"));
-        share = new BoxExternalFile(
+    }
+
+    private BoxExternalFile createShareWitzSize(long shareSize) {
+        return new BoxExternalFile(
             new QblECPublicKey(new byte[0]),
             "prefix",
             "block",
             "name",
-            100L,
-            123L,
-            new byte[0]
-        );
-        share2 = new BoxExternalFile(
-            new QblECPublicKey(new byte[0]),
-            "prefix",
-            "block",
-            "name",
-            200L,
+            shareSize,
             123L,
             new byte[0]
         );
@@ -56,22 +50,24 @@ public class VirtualShareTreeItemTest extends AbstractFxTest {
     @Test
     public void testRefreshesShares() throws Exception {
         ShareNotificationMessage message = new ShareNotificationMessage("http://testurl", "key", "message");
-        sharingService.share = share;
+        sharingService.share = createShareWitzSize(FIRST_SHARE_SIZE);
         notifications.add(message);
         item = createItem();
 
-        waitUntil(() -> !item.getChildren().isEmpty());
-        BoxExternalFile object1 = (BoxExternalFile)item.getChildren().get(0).getValue();
-        assertThat(object1.getSize(), equalTo(100L));
+        assertShareSize(FIRST_SHARE_SIZE);
 
-        sharingService.share = share2;
+        sharingService.share = createShareWitzSize(SECOND_SHARE_SIZE);
         item.refresh();
+        assertShareSize(SECOND_SHARE_SIZE);
+    }
+
+    private void assertShareSize(long size) {
         assertAsync(() -> {
             ObservableList<TreeItem<BoxObject>> children = item.getChildren();
             assertFalse(children.isEmpty());
             BoxExternalFile object2 = (BoxExternalFile) children.get(0).getValue();
             return object2.getSize();
-        }, equalTo(200L));
+        }, equalTo(size));
     }
 
     private VirtualShareTreeItem createItem() {
