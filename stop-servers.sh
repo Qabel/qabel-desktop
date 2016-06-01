84104b81d1d29eb87f058306710c3376e3b0508c
Julian Seeger <seeger.julian@gmail.com>
2016-06-01 11:48:27 +0200

    Update stop-servers.sh

--- stop-servers.sh
+++ stop-servers.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 (cat qabel-drop/drop.pid | xargs kill && rm qabel-drop/drop.pid) || echo "failed to kill drop server"
 (cat qabel-accounting/accounting.pid | xargs kill && rm qabel-accounting/accounting.pid) || echo "failed to kill accounting server"
 (cat qabel-block/block.pid | xargs kill && rm qabel-block/block.pid) || echo "failed to kill block server"
