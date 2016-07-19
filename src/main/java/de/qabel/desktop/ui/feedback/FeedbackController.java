0e3702c64b5eeaca9dcaace069b85a3deb0cd7ca
nerminnicevic <nermin.nicevic@gmail.com>
2016-07-19 13:52:45 +0200

    #307 - added an information dialog that the feedback was sent, text of this dialog are changeable trough the resources

--- src/main/java/de/qabel/desktop/ui/feedback/FeedbackController.java
+++ src/main/java/de/qabel/desktop/ui/feedback/FeedbackController.java
@@ -6,6 +6,7 @@ import de.qabel.desktop.ui.AbstractController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -13,6 +14,7 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 
 public class FeedbackController extends AbstractController implements Initializable {
 
@@ -28,15 +30,17 @@ public class FeedbackController extends AbstractController implements Initializa
 
     @Inject
     private CrashReportHandler reportHandler;
+    private ResourceBundle resourceBundle;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        resourceBundle = resources;
     }
 
     @FXML
     protected void handleSendButtonAction(ActionEvent event) {
         handleSendButtonAction();
+        showPopup();
     }
 
     protected void handleSendButtonAction() {
@@ -50,16 +54,29 @@ public class FeedbackController extends AbstractController implements Initializa
                     feedbackField.setText("");
                     nameField.setText("");
                     emailField.setText("");
-                } catch (IOException  e) {
+                } catch (IOException e) {
                     alert(e);
                 }
             }
+
         }.start();
 
 
-
-
     }
+
+    private void showPopup() {
+        String infoMessage = resourceBundle.getString("feedBackInfoMessage");
+        String titleBar = resourceBundle.getString("feedBackInfoHeader");
+        infoBox(infoMessage, titleBar);
+    }
+
+    private void infoBox(String infoMessage, String titleBar) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titleBar);
+        alert.setContentText(infoMessage);
+        alert.show();
+    }
+
 }
 
 
