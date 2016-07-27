33638a5d0762ce43e82bdf7459ec806381780e4c
nerminnicevic <nermin.nicevic@gmail.com>
2016-07-27 09:47:00 +0200

    removed unnecessary @Before

--- src/test-gui/java/de/qabel/desktop/ui/feedback/FeedbackGuiTest.java
+++ src/test-gui/java/de/qabel/desktop/ui/feedback/FeedbackGuiTest.java
@@ -2,16 +2,13 @@ package de.qabel.desktop.ui.feedback;
 
 import com.airhacks.afterburner.views.FXMLView;
 import de.qabel.desktop.ui.AbstractGuiTest;
-
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FeedbackGuiTest extends AbstractGuiTest<FeedbackController> {
     private FeedbackPage page;
 
     @Override
-    @Before
     public void setUp() throws Exception {
         super.setUp();
         page = new FeedbackPage(baseFXRobot, robot, controller);
