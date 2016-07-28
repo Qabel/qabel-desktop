8de5b937d7cde79f66bf937d67c3b0296c156751
nnice <nermin.nicevic@gmail.com>
2016-07-28 16:28:23 +0200

    #307 - Feedback send dialog (#397)
    * #307 - added an information dialog that the feedback was sent, text of this dialog are changeable trough the resources
    
    * #307 - added controller and gui tests for my implementation
    
    * codecleanup
    
    * fixed Test for feedback controller, waitUntill dont works anymore since got changed into runLater
    
    * optimized testcases for Controller and Gui
    
    * optimized imports
    
    * fixed sendFeedbackShowsThanksDialog, asserting the whole alert object instead of just the dialogpane
    
    * Merge branch 'master' of github.com:Qabel/qabel-desktop into quotaDisplay
    
    clean up
    
    * renamed titleBar into something less missleading
    
    * remove unused
    
    * better message
    
    * improved test for thanksDialog
    
    * removed unnecessary @Before
    
    * removed unnecessary @Before, and import static assertion methods
    
    * removed default  header by setting an empty string
    
    * reverted ContactIntegration.. not part of this issue

---
+++ src/test-gui/java/de/qabel/desktop/ui/feedback/FeedbackPage.java
@@ -0,0 +1,39 @@
+package de.qabel.desktop.ui.feedback;
+
+import com.sun.javafx.robot.FXRobot;
+import de.qabel.desktop.ui.AbstractPage;
+import javafx.scene.Node;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import org.testfx.api.FxRobot;
+
+class FeedbackPage extends AbstractPage {
+    private FeedbackController controller;
+
+    FeedbackPage(FXRobot baseFXRobot, FxRobot robot, FeedbackController controller) {
+        super(baseFXRobot, robot);
+        this.controller = controller;
+    }
+
+    void sendFeedback() {
+        typeFeedbackInformations();
+        clickOn(controller.submitButton);
+    }
+
+    void confirmAlertBox() {
+        Node button = getDialogPane().lookupButton(ButtonType.OK);
+        clickOn(button);
+    }
+
+    DialogPane getDialogPane() {
+        return controller.alert.getDialogPane();
+    }
+
+
+    private void typeFeedbackInformations() {
+        clickOn(controller.nameField).write("guitest nameField");
+        clickOn(controller.emailField).write("guitest emailField");
+        clickOn(controller.feedbackField).write("guitest feedbackField");
+
+    }
+}
