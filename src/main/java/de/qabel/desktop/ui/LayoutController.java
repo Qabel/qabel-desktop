abf1a14a8481378a3d4755f854c793c31574d0d3
Jens Kadenbach <gitdax@daxbau.net>
2016-09-26 14:58:41 +0200

    Link to account-upgrade website

--- src/main/java/de/qabel/desktop/ui/LayoutController.java
+++ src/main/java/de/qabel/desktop/ui/LayoutController.java
@@ -27,6 +27,8 @@ import de.qabel.desktop.ui.transfer.ComposedProgressBar;
 import de.qabel.desktop.ui.transfer.TransferViewModel;
 import de.qabel.desktop.ui.util.Icons;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -44,6 +46,7 @@ import javax.inject.Inject;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
@@ -127,6 +130,7 @@ public class LayoutController extends AbstractController implements Initializabl
     NaviItem syncNav;
     NaviItem accountingNav;
     NaviItem aboutNav;
+    NaviItem upgradeNav;
     public QuotaState quotaState;
 
     @FXML
@@ -224,6 +228,15 @@ public class LayoutController extends AbstractController implements Initializabl
         aboutNav = createNavItem(resourceBundle.getString("layoutAbout"),
             Icons.getIcon(INFO),
             aboutView);
+        upgradeNav = createNavItem(resourceBundle.getString("upgrade"),  Icons.getIcon(Icons.LINK),
+            ((event) -> executor.submit(() -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://qabel.de/de/qabelnow"));
+            } catch (IOException | URISyntaxException e) {
+                alert(e);
+            }
+        })));
+        navi.getChildren().add(upgradeNav);
     }
 
     void fillQuotaInformation(QuotaState quotaState) {
@@ -341,6 +354,13 @@ public class LayoutController extends AbstractController implements Initializabl
     private NaviItem createNavItem(String label, Image image, FXMLView view) {
         NaviItem naviItem = new NaviItem(label, image);
         linkNaviItem(view, naviItem);
+        return naviItem;
+    }
+
+    private NaviItem createNavItem(String label, ImageView image, EventHandler<ActionEvent> callback) {
+        NaviItem naviItem = new NaviItem(label, image);
+        naviItem.setOnAction(callback);
+        navi.getChildren().add(naviItem);
         return naviItem;
     }
 
