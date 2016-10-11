45aea73fec63683351323e17df9bfb703b6f6af4
nerminnicevic <nermin.nicevic@gmail.com>
2016-10-11 14:23:33 +0200

    add InlineCssTextArea to style the textarea better

--- src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
+++ src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
@@ -2,10 +2,8 @@ package de.qabel.desktop.ui.actionlog.item.renderer;
 
 import de.qabel.desktop.daemon.drop.TextMessage;
 import javafx.scene.Node;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.StackPane;
+import org.fxmisc.richtext.InlineCssTextArea;
 
 import java.util.ResourceBundle;
 
@@ -15,26 +13,16 @@ public class PlaintextMessageRenderer implements FXMessageRenderer {
     public Node render(String dropPayload, ResourceBundle resourceBundle) {
         Label label = new Label(renderString(dropPayload, resourceBundle));
         label.getStyleClass().add("message-text");
-        return makeSelectable(label);
+
+        return createTransparentBackgroundTextArea(label);
     }
 
-    private Label makeSelectable(Label label) {
-        StackPane textStack = new StackPane();
-        textStack.getStyleClass().add("selectable-text");
-
-        TextArea textField = new TextArea(label.getText());
-        textField.setEditable(false);
-        textField.setPrefRowCount(1);
-        // the invisible label is a hack to get the textField to size like a label.
-        Label invisibleLabel = new Label();
-        invisibleLabel.textProperty().bind(label.textProperty());
-        invisibleLabel.setVisible(false);
-        textStack.getChildren().addAll(invisibleLabel, textField);
-
-        label.textProperty().bindBidirectional(textField.textProperty());
-        label.setGraphic(textStack);
-        label.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-        return label;
+    private InlineCssTextArea createTransparentBackgroundTextArea(Label label) {
+        InlineCssTextArea textArea = new InlineCssTextArea(label.getText());
+        textArea.setWrapText(true);
+        textArea.getStyleClass().add("message-text");
+        textArea.setEditable(false);
+        return textArea;
     }
 
     @Override
