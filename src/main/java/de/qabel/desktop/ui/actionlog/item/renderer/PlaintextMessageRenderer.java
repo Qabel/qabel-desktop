5640c74a81dc2de6a45286ad727f7b3912a148ef
nerminnicevic <nermin.nicevic@gmail.com>
2016-10-12 15:53:27 +0200

    cleanup and removed scenic view,  add forgot selector and definitions in css

--- src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
+++ src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
@@ -19,20 +19,23 @@ public class PlaintextMessageRenderer implements FXMessageRenderer {
         label.getStyleClass().add("message-text");
 
         return makeSelectable(label);
+//        return label;
     }
 
-    private Label makeSelectable(final Label label) {
+    private Label makeSelectable(Label label) {
         StackPane textStack = new StackPane();
-        final TextArea textArea = new TextArea();
+        TextArea textArea = new TextArea();
         textArea.getStyleClass().addAll(label.getStyleClass());
         textArea.textProperty().bind(label.textProperty());
         textArea.setEditable(false);
         textArea.setPrefRowCount(1);
 
         Platform.runLater(() -> {
-            textArea.setPrefSize(label.getWidth(), label.getHeight());
+            if (label.getHeight() > textArea.getHeight()) {
+                textArea.setPrefHeight(label.getHeight() + 20);
+            }
+//            textArea.setPrefWidth(label.getWidth());
         });
-
         // the invisible label is a hack to get the textField to size like a label (maybe it is not really necessary...)
         Label invisibleLabel = new Label();
         invisibleLabel.setWrapText(false);
