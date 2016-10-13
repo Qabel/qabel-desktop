94b2688cd758dc4ae6e35d0862c4f1cf83e3f83d
nerminnicevic <nermin.nicevic@gmail.com>
2016-10-13 16:33:02 +0200

    generation and searching for uri via regex and replace them with the delimiters of HyperLinkLabel that he can build clickable links out of it. and add the eventlistener to open the link in browser

--- src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
+++ src/main/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRenderer.java
@@ -2,15 +2,66 @@ package de.qabel.desktop.ui.actionlog.item.renderer;
 
 import de.qabel.desktop.daemon.drop.TextMessage;
 import javafx.scene.Node;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.text.TextFlow;
+import org.controlsfx.control.HyperlinkLabel;
+import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PlaintextMessageRenderer implements FXMessageRenderer {
+    private static final String STYLE_CLASS = "message-text";
+    public Consumer<String> browserOpener;
+
+    public PlaintextMessageRenderer() {
+        browserOpener = (uri) -> {
+            try {
+                Desktop.getDesktop().browse(new URI(uri));
+            } catch (IOException | URISyntaxException ignored) {
+            }
+        };
+
+    }
+
     @Override
     public Node render(String dropPayload, ResourceBundle resourceBundle) {
-        Label label = new Label(renderString(dropPayload, resourceBundle));
-        label.getStyleClass().add("message-text");
+        String text = renderString(dropPayload, resourceBundle);
+        return renderHyperlinks(text);
+    }
+
+    @NotNull
+    TextFlow renderHyperlinks(String message) {
+        String regex = "(https?:\\/\\/(?:www\\.|(?!www))[^\\s\\.]+\\.[^\\s]{2,}|www\\.[^\\s]+\\.[^\\s]{2,})";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.COMMENTS | Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(message);
+        String result = matcher.replaceAll("[$1]");
+
+        HyperlinkLabel hyperLinkLabel = new HyperlinkLabel(result);
+        hyperLinkLabel.getStyleClass().add("text");
+
+        hyperLinkLabel.setOnAction(event -> {
+            Hyperlink link = (Hyperlink) event.getSource();
+            final String uri = link == null ? "" : link.getText();
+            new Thread(() -> browserOpener.accept(uri)).start();
+        });
+
+        TextFlow node = new TextFlow(hyperLinkLabel);
+        node.getStyleClass().add(STYLE_CLASS);
+        return node;
+    }
+
+    @NotNull
+    private Label renderLabel(String text) {
+        Label label = new Label(text);
+        label.getStyleClass().add(STYLE_CLASS);
         return label;
     }
 
@@ -18,4 +69,6 @@ public class PlaintextMessageRenderer implements FXMessageRenderer {
     public String renderString(String dropPayload, ResourceBundle resourceBundle) {
         return TextMessage.fromJson(dropPayload).getText();
     }
+
+
 }
--- src/test/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRendererTest.java
+++ src/test/java/de/qabel/desktop/ui/actionlog/item/renderer/PlaintextMessageRendererTest.java
@@ -3,33 +3,53 @@ package de.qabel.desktop.ui.actionlog.item.renderer;
 import de.qabel.desktop.ui.AbstractFxTest;
 import javafx.scene.Node;
 import javafx.scene.control.Labeled;
+import javafx.scene.text.TextFlow;
+import org.controlsfx.control.HyperlinkLabel;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PlaintextMessageRendererTest extends AbstractFxTest {
     private PlaintextMessageRenderer renderer;
+    private String payload;
 
     @Before
     public void setUp() throws Exception {
         renderer = new PlaintextMessageRenderer();
+        payload = "{'msg': 'content'}";
     }
 
     @Test
     public void rendersPlaintextFromJson() {
-        String payload =  "{'msg': 'content'}";
         String message = renderer.renderString(payload, null);
-
         assertEquals("content", message);
     }
 
     @Test
     public void rendersMessageNode() {
-        String payload =  "{'msg': 'content'}";
         Node node = renderer.render(payload, null);
         assertTrue(node instanceof Labeled);
-        assertEquals("content", ((Labeled)node).getText());
+        assertEquals("content", ((Labeled) node).getText());
     }
+
+    @Test
+    public void renderHyperlinks() throws Exception {
+        final String string = "This is a Text,\n"
+            + " wich has a neat hyperlinks www.qabel.de";
+
+        String expectedUriFormat = "[www.qabel.de]";
+
+        AtomicReference<String> browserOpener = new AtomicReference<>();
+        renderer.browserOpener = browserOpener::set;
+
+        Node node = renderer.renderHyperlinks(string);
+        HyperlinkLabel hyperLinkLabel = ((HyperlinkLabel) ((TextFlow) node).getChildren().get(0));
+        assertTrue(hyperLinkLabel.getText().contains(expectedUriFormat));
+    }
+
+
 }
